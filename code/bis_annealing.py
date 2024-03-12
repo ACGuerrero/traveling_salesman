f886b1c4172d15a24ffa97cc1f570b2ec74b195b
ACGuerrero <castillo.g.adan@gmail.com>
2024-03-12 12:09:36 +0200

    heap algorithm now gives us a numpy array

--- code/bis_annealing.py
+++ code/bis_annealing.py
@@ -1,8 +1,15 @@
 import numpy as np 
-from heap_algorithm import generate_permutations
+from good_heap import *
 from cities import cities_data
 
 def earth_distance(lat1, lon1, lat2, lon2):
+    '''
+    This function calculates the distance between two points
+    on earth using spherical coordinates.
+
+    Input: floats, latitudes and longitudes in degrees
+    Output: float, distance in km
+    '''
     # earth rayon
     R = 6371.0
 
@@ -27,17 +34,23 @@ def itinerary_distance(itinerary):
     output : float, distance in km
     '''
     distance = 0
+
     for i in range(len(itinerary)-1):
         lat1, lon1 = itinerary[i]['latitude'], itinerary[i]['longitude']
         lat2, lon2 = itinerary[i + 1]['latitude'], itinerary[i + 1]['longitude']
         distance += earth_distance(lat1, lon1, lat2, lon2)
+
     return distance
 
 
-
-
-
 def greedy_optimization(city_list):
+    '''
+    This function performs the greedy optimization to obtain the best itinerary
+    for the traveling salesman problem.
+
+    Input: list, of dictionaries with information about cities
+    Output: tuple, list with names of cities in order and float with distance in km
+    '''
     # Generate permutations
     itineraries = []
     generate_permutations(city_list, len(city_list), itineraries)
@@ -54,6 +67,7 @@ def greedy_optimization(city_list):
 
 
 def move_city_to_start(city_list, starting_city):
+
     city_index = next(index for (index, city) in enumerate(city_list) if city['name'] == starting_city)
     city = city_list.pop(city_index)
     city_list.insert(0, city)
@@ -77,10 +91,31 @@ def greedy_optimization_with_starting_point(city_list, starting_city):
     best_itinerary =  [city['name'] for city in itineraries[index]]
     return best_itinerary, distances[index]
 
+def metropolis_step(beta,itinerary):
+    '''
+    This function performs a step of the Metropolis-Hastings algorithm.
+
+    Input: float and list, beta parameter and itinerary
+    Output: list, selected itinerary
+    '''
+    distance = itinerary_distance(itinerary)
+    new_itinerary = np.random.permutation(itinerary)
+    new_distance = itinerary_distance(new_itinerary)
+
+    if  new_distance < distance:
+        return new_itinerary
+    else:
+        boltzmann_factor = np.exp(-beta*new_distance)
+        tirage = np.random.binomial(1,boltzmann_factor)
+        if tirage == 1 :
+            return new_itinerary
+        else:
+            return itinerary
+
 
 if __name__ == '__main__':
     # Recover french cities
-    cities_france = [city for city in cities_data["cities"] if city.get("country") == "France"]
+    cities_france = np.array([city for city in cities_data["cities"] if city.get("country") == "France"])
     
     # First we run the greedy algorithm
     greedy_itinerary, greedy_distance = greedy_optimization(cities_france)
---
+++ good_heap.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def heaps_algorithm(A,k,result):
+    if k == 1:
+        result.append(list(A))
+    else:
+        heaps_algorithm(A,k - 1, result)
+        for i in range(k - 1):
+            if k % 2 == 0:
+                A[i], A[k-1] = A[k-1], A[i]
+            else:
+                A[0], A[k-1] = A[k-1], A[0]
+            heaps_algorithm(A, k - 1, result)
+
+def generate_permutations(A,k):
+    result = []
+    heaps_algorithm(A,k,result)
+    return np.array(result)
+
+
+if __name__ == '__main__':
+    # Just an example
+    array = [1, 2, 3, 4]
+    permutations = generate_permutations(array,len(array))
+    print('Voici toutes les permutations: \n')
+    for permutation in permutations:
+        print(permutation)
+    print(f'Number of permutations is {len(permutations)}, and type of array is {type(permutations)}')
